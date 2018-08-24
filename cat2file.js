960a9f131866cd76cd5473b97eccf3127799936a
Melissa Cantu <meli_cantu12@hotmail.com>
2018-08-24 11:09:05 -0500

    problem cat

--- cat2file.js
+++ cat2file.js
@@ -4,25 +4,78 @@ var fs = require('fs');
 var concat = require('concat-files');
 
 // 3)
-inputFile1 = 'hello.txt';
-inputFile2 = 'world.txt';
+let inputFile1 = 'hello1.txt';
+let inputFile2 = 'world1.txt';
+let catFiles;
+
 
 function combineFiles (inputFile1, inputFile2){
+
     var promise = new Promise(function (resolve, reject) {
 
-        input = fs.appendFile(inputFile1, inputFile2, function(err){
-            if (err) reject(err);
-        })
 
-        fs.readFile(input, function(err){
-            if (err) reject(err);
-        })
+        try{
 
-        fs.writeFile('output.txt', input, function(err){
-            if (err) reject(err);
-        })
+            fs.readFile(inputFile1, 'utf8', function(err, data){
+                if (err){
+                    reject(err)
+                }
+                else{
+                    catFile = data;
+                    resolve(data);
+                }
+            })
+
+
+            // input = fs.appendFile(inputFile1, inputFile2, function(err){
+            
+            //     if (err){
+            //         reject(err);
+            //     }
+            //     console.log('input', input);
+            //     resolve(input);
+                
+            // });
+        }
+        catch(error){
+            reject(error)
+        }
+
+        
+
+        // // 
+
+        // resolve('hello world');
+
     })
+    .then (function(results){
+        
+        fs.readFile(inputFile2, 'utf8', function(err, data){
+            
+                catFile += data;
 
+                return results;
+           
+        })
+            // fs.readFile(input, function(err){
+            //     if (err) reject(err);
+            
+    })
+    .then (function(results){
+
+        console.log(catFile);
+        
+        fs.writeFile('output.txt', catFile, 'utf8', function(err){
+            if (err) reject(err);
+        })
+    }).catch(function(err) {
+        console.log("error here: " + err);
+    });
+    
+    return promise;
+}
+
+combineFiles(inputFile1, inputFile2);
 //         fs.writeFile(output.txt, url, function(err){
 //             if (err) reject(err);
 
@@ -31,7 +84,9 @@ function combineFiles (inputFile1, inputFile2){
 // }).catch(function(err) {
 //     console.log("error here: " + err);
 // })
-}
+
+    
+//}
 
 
 app.listen(3000);
